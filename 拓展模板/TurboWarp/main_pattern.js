188b087f0017e713cbb0daab38a81d1410cb9c6b
six-6 <1195132772@qq.com>
2022-02-24 22:18:59 +0800

    拓展:增加TurboWarp拓展模板

---
+++ 拓展模板/TurboWarp/main_pattern.js
@@ -0,0 +1,53 @@
+
+// 在前面粘贴 TFGS.js 的内容
+
+var _enableTFGS = true;
+
+class TFGS {
+    getInfo() {
+        TFGS();
+        TFGSON();
+        return {
+            id: 'TFGS',
+            name: 'TFGS',
+
+            blocks: [
+                {
+                    opcode: 'turnon',
+                    blockType: Scratch.BlockType.BUTTON,
+                    text: '打开 TFGS'
+                },
+                {
+                    opcode: 'turnoff',
+                    blockType: Scratch.BlockType.BUTTON,
+                    text: '关闭 TFGS'
+                }
+            ]
+        };
+    }
+
+    turnon() {
+        if (!_enableTFGS) {
+            try {
+                TFGSON();
+                _enableTFGS = true;
+                alert("TFGS 已打开")
+            } catch (err) {
+                alert("错误: " + err.message);
+            }
+        }
+    }
+    turnoff() {
+        if (!_enableTFGS) {
+            try {
+                TFGSOFF();
+                _enableTFGS = false;
+                alert("TFGS 已关闭")
+            } catch (err) {
+                alert("错误: " + err.message);
+            }
+        }
+    }
+}
+
+Scratch.extensions.register(new TFGS());
