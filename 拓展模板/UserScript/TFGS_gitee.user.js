d5aed467c8524111f8a3651681ce95fbe5a91c5c
six-6 <1195132772@qq.com>
2022-02-26 10:29:27 +0800

    增加(拓展us):UserScript拓展
    目前只在via上测试。

---
+++ 拓展模板/UserScript/TFGS_gitee.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==  
+// @name         TFGS  
+// @author       1195132772@qq.com  
+// @namespace    https://gitee.com/six-6/TFGS  
+// @description  Scratch 3 增强  
+// @include      *  
+// ==/UserScript==  
+setTimeout(function(){
+	var s = document.createElement('script');
+	s.src = 'https://gitee.com/six-6/tfgs/raw/master/TFGS.js';
+	document.body.appendChild(s);
+},0);
