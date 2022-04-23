5994d649bf55478e35095d0602c4d5362eceb529
six-6 <1195132772@qq.com>
2022-04-23 16:26:08 +0800

    inspect增加查看get内容

--- inspect.js
+++ inspect.js
@@ -37,7 +37,7 @@ function inspect(x) {
 			if (t === "function") {
 				//str += JSON.stringify(x.name);
 				str += x.toString().replace(/\n/g, "\n  " + ind);
-				listAttr(x);
+				listAttr(x, x);
 			}
 			if (t === "object") {
 				if (typeof x.constructor === "function") {
@@ -49,55 +49,67 @@ function inspect(x) {
 				} else {
 					str += "??????";
 				}
-				listAttr(x);
+				listAttr(x, x);
 			}
 
-			function listAttr(x) {
-				if (u.includes(x)) {
+			function listAttr(xx, x) {
+				let hide = false;
+				if (u.includes(xx)) {
 					str += " [goto __" + (u.indexOf(x) + 1) + "__]";
 					return;
 				}
-				u.push(x);
-				str += " [__" + (u.length) + "__]";
-				let y = Object.getOwnPropertyDescriptors(x);
-				let z = Object.getOwnPropertyNames(x)
-					.concat(Object.getOwnPropertySymbols(x));
-				if (z.length > 200) {
-					str += "\n  " + ind + z.length + "items";
-				} else {
-					for (let j in z) {
-						let i = z[j];
-						str += "\n  " + ind;
-						str += y[i].configurable ? "-" : "x";
-						str += y[i].enumerable ? "-" : "+";
-						str += " ";
-						str += typeof i === "symbol" ? "Symbol " + JSON.stringify(i.description) : JSON.stringify(i);
-						str += " = ";
-						try {
-							if ("get" in y[i]) {
-								str += "\n  " + ind + " |- get = ";
-								inspect2(y[i].get, ind + " |  ");
-								str += "\n  " + ind + " |- set = ";
-								inspect2(y[i].set, ind + " |  ");
-							} else {
-								inspect2(y[i].value, ind + "  ");
+				while (true) {
+					if (u.includes(x))
+						hide = true;
+					else
+						u.push(x);
+					str += " [__" + (u.length) + "__]";
+					let y = Object.getOwnPropertyDescriptors(x);
+					let z = Object.getOwnPropertyNames(x)
+						.concat(Object.getOwnPropertySymbols(x));
+					if (z.length > 200) {
+						str += "\n  " + ind + z.length + "items";
+					} else {
+						for (let j in z) {
+							let i = z[j];
+							try {
+								if ("get" in y[i]) {
+									str += "\n  " + ind;
+									str += y[i].configurable ? "-" : "x";
+									str += y[i].enumerable ? "-" : "+";
+									str += " ";
+									str += typeof i === "symbol" ? "Symbol " + JSON.stringify(i.description) : JSON.stringify(i);
+									str += " ~ ";
+									inspect2(xx[i], ind + "  ");
+									str += "\n  " + ind + "  ~~ get = ";
+									inspect2(y[i].get, ind + "    ");
+									str += "\n  " + ind + "  ~~ set = ";
+									inspect2(y[i].set, ind + "    ");
+								} else if (!hide) {
+									str += "\n  " + ind;
+									str += y[i].configurable ? "-" : "x";
+									str += y[i].enumerable ? "-" : "+";
+									str += " ";
+									str += typeof i === "symbol" ? "Symbol " + JSON.stringify(i.description) : JSON.stringify(i);
+									str += " = ";
+									inspect2(y[i].value, ind + "  ");
+								}
+							} catch (err) {
+								str += "ERR: " + err.message;
 							}
-						} catch (err) {
-							str += "ERR: " + err.message;
 						}
 					}
-				}
-				if (typeof x.constructor === "function" && x !== x.constructor.prototype) {
-					str += "\n  " + ind + "== From " + JSON.stringify(x.constructor.name) + " ==";
-					if (!u.includes(x.constructor.prototype)) {
-						listAttr(x.constructor.prototype);
-					} else if (x.constructor.prototype !== x) {
-						str += " [goto __" + (u.indexOf(x.constructor.prototype) + 1) + "__]";
+					if (typeof x.constructor === "function" && x !== x.constructor.prototype) {
+						str += "\n  " + ind + "== From " + JSON.stringify(x.constructor.name) + " ==";
+						if (u.includes(x.constructor.prototype)) {
+							str += " [goto __" + (u.indexOf(x.constructor.prototype) + 1) + "__]";
+						}
+						x = x.constructor.prototype;
+					} else {
+						return;
 					}
-				} else {
-					//str += "??????";
+					//u.pop();
 				}
-				//u.pop();
 			}
 		} else {
 			if (t === "number") {
