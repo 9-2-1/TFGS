aabb1d4a0d19f2bccb9e64fab30c54e8b9984c99
six-6 <1195132772@qq.com>
2022-05-02 10:57:33 +0800

    object_format函数以及测试

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 let result = "";
 
-function test(a, b, c) {
+function test(n, a, b) {
 	if (a !== b) {
-		result += c + ":" + JSON.stringify(a) + "!=" + JSON.stringify(b) + "\n";
+		result += n + ":" + a + "=" + JSON.stringify(a) + "!=" + b + "=" + JSON.stringify(b) + "\n";
 	}
 }
 
@@ -25,10 +25,34 @@ function element(tagname, classname, type) {
 }
 
 try {
-	tfgs.log.add("test", "#0202ff", "i  have been <b>fjej</b>,once.");
-	tfgs.log.add("test", "#000000", "i  have been <b>fjej</b>,once.");
-	tfgs.log.add("data", "#ff0000", "caps");
-	tfgs.log.add("data", "#000000", "lock");
+	// object_format
+	test("of001", object_format("a", "string"), "a");
+	test("of002", object_format(123, "string"), "123");
+	test("of003", object_format(123, "string!"), "");
+	test("of004", object_format(123, "string?"), undefined);
+	test("of005", object_format(true, "string"), "true");
+	test("of006", object_format("1234", "number"), 1234);
+	test("of007", Object.is(object_format("abc", "number"), NaN), true);
+	test("of008", object_format("1234", "number!"), 0);
+	test("of009", object_format("a", "boolean"), true);
+	test("of010", object_format("a", "boolean!"), false);
+	test("of011", object_format("a", "boolean?"), undefined);
+	test("of012", JSON.stringify(object_format([1, 2, 3], "object")), "{\"0\":1,\"1\":2,\"2\":3}");
+	test("of013", JSON.stringify(object_format({
+		a: 1,
+		b: 2,
+		c: {
+			d: "a",
+			f: true
+		}
+	}, {
+		a: "!",
+		b: "0!",
+		c: {
+			e: "false!",
+			f: "string!"
+		}
+	})), "{\"a\":\"\",\"b\":2,\"c\":{\"e\":false,\"f\":\"\"}}");
 	let elog = element("div");
 	tfgs.log.display(elog, {
 		"name": null,
@@ -37,5 +61,5 @@ try {
 	document.body.appendChild(elog);
 	fin();
 } catch (e) {
-	err(e);
+	err(e.message);
 }
