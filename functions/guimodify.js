3472d593884cf237855bbe9d7bc17cdf45f92121
six-6 <1195132772@qq.com>
2022-05-11 12:09:27 +0800

    折叠舞台元素拓展(4): 合并函数，整理

--- functions/guimodify.js
+++ functions/guimodify.js
@@ -1,11 +1,10 @@
 ! function() {
 	let api;
-	let loadfinish = false;
 	let interval = -1;
 
-	let pelement = {};
 	let pbutton = {};
 	let foption = {};
+	let Bfoldmenu = undefined;
 
 	function getRulesList() {
 		let sss = document.styleSheets;
@@ -46,246 +45,153 @@
 		return document.querySelector(`[class^=${namebegin}],[class*= ${namebegin}]`);
 	}
 
-	function createButton(element, offx, offy) {
-		let button = document.createElement("span");
-		button.classList.add(selectClass("button_outlined-button_"));
-		button.classList.add(selectClass("stage-header_stage-button_"));
-		button.classList.add("tfgsGuimodifyButton");
-		button.style.left = offx;
-		button.style.top = offy;
-		element.appendChild(button);
-		return button;
-	}
+			function configButton(options) {
+				let buttonid = options.buttonid;
+				let enable = options.enable;
+				let addinner = options.addinner;
+				let removeinner = options.removeinner;
+				let styles = options.styles;
+				let targetcss = options.targetcss;
+				let cssname = options.cssname;
+				if (enable /*foption.foldmenu*/ ) {
+					/* ------ on ------ */
+					if (pbutton[buttonid] === undefined) {
+						let target = selectElement(targetcss /*"gui_menu-bar-position_"*/ );
+						if (target === null) {
+							api.warn(buttonid + ": target not found.");
+							return;
+						}
+						let button = document.createElement("span");
+						button.classList.add(selectClass("button_outlined-button_"));
+						button.classList.add(selectClass("stage-header_stage-button_"));
+						button.classList.add("tfgsGuimodifyButton");
+						for (let i in styles)
+							button.style[i] = styles[i];
+						button.innerText = addinner;
+						button.checked = false;
+						button.addEventListener("click", function() {
+							if (button.checked) {
+								document.body.classList.remove(cssname /*"tfgsGuimodifyMenubarFold"*/ );
+								button.innerText = addinner;
+							} else {
+								document.body.classList.add(cssname);
+								button.innerText = removeinner;
+							}
+							button.checked ^= true;
+							dispatchEvent(new Event("resize"));
+						});
+						target.appendChild(button);
+						if (options.clickaftercreate)
+							button.click();
+						pbutton[buttonid] = button;
+						api.info(buttonid + ": OK");
+					}
+				} else {
+					/* ------ off ------ */
+					if (pbutton[buttonid] !== undefined) {
+						pbutton[buttonid].remove();
+						pbutton[buttonid] = undefined;
+					}
+					document.body.classList.remove(cssname);
+					dispatchEvent(new Event("resize"));
+					api.info(buttonid + ": OFF");
+				}
+			}
 
 	function updateStatus() {
 		try {
 
-			if (foption.foldmenu) {
-				/* ------ foldmenu:on ------ */
-				if (pelement.menubar === undefined) {
-					let menubar = selectElement("gui_menu-bar-position_");
-					let bodywrap = selectElement("gui_body-wrapper_");
-					if (menubar !== null && bodywrap !== null) {
-						if (pelement.foldmenubutton === undefined) {
-							let button = pelement.foldmenubutton = createButton(menubar, "0", "100%");
-							pbutton.foldmenubutton = false;
-							button.innerText = "≡";
-							button.addEventListener("click", function() {
-								if (!pbutton.foldmenubutton) {
-									button.innerText = "≡";
-									menubar.classList.add("tfgsGuimodifyMenubarFold");
-									bodywrap.classList.add("tfgsGuimodifyMenubarBody");
-								} else {
-									button.innerText = "▲";
-									menubar.classList.remove("tfgsGuimodifyMenubarFold");
-									bodywrap.classList.remove("tfgsGuimodifyMenubarBody");
-								}
-								pbutton.foldmenubutton = !pbutton.foldmenubutton;
-								dispatchEvent(new Event("resize"));
-							});
-							button.click();
-							api.log("menubar is OK");
-						}
-						pelement.menubar = menubar;
-						pelement.bodywrap = bodywrap;
-					} else {
-						api.warn("menubar not found.");
-					}
-				}
-			} else {
-				/* ------ foldmenu:off ------ */
-				if (pelement.menubar !== undefined) {
-					let menubar = pelement.menubar;
-					let bodywrap = pelement.bodywrap;
-					if (pelement.foldmenubutton !== undefined) {
-						pelement.foldmenubutton.remove();
-						delete pelement.foldmenubutton;
-					}
-					menubar.classList.remove("tfgsGuimodifyMenubarFold");
-					bodywrap.classList.remove("tfgsGuimodifyMenubarBody");
+			let sel = selectElement("sprite-selector_scroll-wrapper_");
+			if (sel !== null)
+				if (foption.foldspriteinfo ||
+					foption.foldstagebutton ||
+					foption.foldthestage)
+					sel.style.position = "relative";
+				else
+					sel.style.position = "inherit";
+
+			if (foption.expand100) {
+				if (!document.body.classList.contains("tfgsGuimodifyExpand100")) {
+					document.body.classList.add("tfgsGuimodifyExpand100");
 					dispatchEvent(new Event("resize"));
-					delete pelement.menubar;
-					delete pelement.bodywrap;
-					api.log("menubar removed");
-				}
-			}
-
-			/* ----------------- */
-
-			if (foption.foldstagetarget) {
-				/* ------ foldstagetarget:on ------ */
-				if (pelement.stagetarget === undefined) {
-					let stagetarget = selectElement("gui_stage-and-target-wrapper_");
-					let buttpos = selectElement("gui_editor-wrapper_");
-					if (stagetarget !== null) {
-						if (pelement.foldstagetargetbutton === undefined) {
-							let button = pelement.foldstagetargetbutton = createButton(buttpos, "0", "0.3rem");
-							pbutton.foldstagetargetbutton = false;
-							button.style.left = "unset";
-							button.style.right = "0";
-							button.innerText = "≫";
-							button.addEventListener("click", function() {
-								if (!pbutton.foldstagetargetbutton) {
-									button.innerText = "≪";
-									stagetarget.classList.add("tfgsGuimodifyStagetargetHide");
-								} else {
-									button.innerText = "≫";
-									stagetarget.classList.remove("tfgsGuimodifyStagetargetHide");
-								}
-								pbutton.foldstagetargetbutton = !pbutton.foldstagetargetbutton;
-								dispatchEvent(new Event("resize"));
-							});
-							api.log("stagetarget is OK");
-						}
-						pelement.stagetarget = stagetarget;
-					} else {
-						api.warn("stagetarget not found.");
-					}
 				}
 			} else {
-				/* ------ foldstagetarget:off ------ */
-				if (pelement.stagetarget !== undefined) {
-					let stagetarget = pelement.stagetarget;
-					if (pelement.foldstagetargetbutton !== undefined) {
-						pelement.foldstagetargetbutton.remove();
-						delete pelement.foldstagetargetbutton;
-					}
-					stagetarget.classList.remove("tfgsGuimodifyStagetargetHide");
+				if (document.body.classList.contains("tfgsGuimodifyExpand100")) {
+					document.body.classList.remove("tfgsGuimodifyExpand100");
 					dispatchEvent(new Event("resize"));
-					delete pelement.stagetarget;
-					api.log("stagetarget removed");
 				}
 			}
 
-			/* ----------------- */
+			configButton({
+				buttonid: "foldmenubar",
+				enable: foption.foldmenu,
+				styles: {
+					left: 0,
+					top: "100%",
+					position: "absolute"
+				},
+				addinner: "▲",
+				removeinner: "▼",
+				targetcss: "gui_menu-bar-position_",
+				cssname: "tfgsGuimodifyMenubarFold",
+				clickaftercreate: true
+			});
 
-			if (foption.foldspriteinfo) {
-				/* ------ foldspriteinfo:on ------ */
-				if (pelement.spriteinfo === undefined) {
-					let spriteinfo = selectElement("sprite-info_sprite-info_");
-					if (spriteinfo !== null) {
-						if (pelement.foldspriteinfobutton === undefined) {
-							let button = pelement.foldspriteinfobutton = createButton(spriteinfo.nextSibling, "0", "0");
-							pbutton.foldspriteinfobutton = false;
-							button.innerText = "△";
-							button.addEventListener("click", function() {
-								if (!pbutton.foldspriteinfobutton) {
-									button.innerText = "▽";
-									spriteinfo.classList.add("tfgsGuimodifySpriteinfoFold")
-								} else {
-									button.innerText = "△";
-									spriteinfo.classList.remove("tfgsGuimodifySpriteinfoFold")
-								}
-								pbutton.foldspriteinfobutton = !pbutton.foldspriteinfobutton;
-							});
-							api.log("spriteinfo is OK");
-						}
-						pelement.spriteinfo = spriteinfo;
-					} else {
-						api.warn("spriteinfo not found.");
-					}
-				}
-			} else {
-				/* ------ foldspriteinfo:off ------ */
-				if (pelement.spriteinfo !== undefined) {
-					let spriteinfo = pelement.spriteinfo;
-					if (pelement.foldspriteinfobutton !== undefined) {
-						pelement.foldspriteinfobutton.remove();
-						delete pelement.foldspriteinfobutton;
-					}
-					spriteinfo.classList.remove("tfgsGuimodifySpriteinfoFold")
-					delete pelement.spriteinfo;
-					api.log("spriteinfo removed");
-				}
-			}
+			configButton({
+				buttonid: "foldstagetarget",
+				enable: foption.foldstagetarget,
+				styles: {
+					right: 0,
+					top: "0.3rem"
+					position: "absolute"
+				},
+				addinner: "▶",
+				removeinner: "◀",
+				targetcss: "gui_editor-wrapper_",
+				cssname: "tfgsGuimodifyStagetargetFold"
+			});
 
-			/* ----------------- */
-			if (foption.foldstagebutton) {
-				/* ------ foldstagebutton:on ------ */
-				if (pelement.stagebutton === undefined) {
-					let stagebutton = selectElement("target-pane_stage-selector-wrapper_");
-					if (stagebutton !== null) {
-						if (pelement.foldstagebuttonbutton === undefined) {
-							let button = pelement.foldstagebuttonbutton = createButton(MARK1 = stagebutton.previousSibling, "0", "0");
-							button.style.left = "unset";
-							button.style.right = "0";
-							pbutton.foldstagebuttonbutton = false;
-							button.innerText = "▷";
-							button.addEventListener("click", function() {
-								if (!pbutton.foldstagebuttonbutton) {
-									button.innerText = "◁";
-									stagebutton.classList.add("tfgsGuimodifyStagebuttonFold")
-								} else {
-									button.innerText = "▷";
-									stagebutton.classList.remove("tfgsGuimodifyStagebuttonFold")
-								}
-								pbutton.foldstagebuttonbutton = !pbutton.foldstagebuttonbutton;
-							});
-							api.log("stagebutton is OK");
-						}
-						pelement.stagebutton = stagebutton;
-					} else {
-						api.warn("stagebutton not found.");
-					}
-				}
-			} else {
-				/* ------ foldstagebutton:off ------ */
-				if (pelement.stagebutton !== undefined) {
-					let stagebutton = pelement.stagebutton;
-					if (pelement.foldstagebuttonbutton !== undefined) {
-						pelement.foldstagebuttonbutton.remove();
-						delete pelement.foldstagebuttonbutton;
-					}
-					stagebutton.classList.remove("tfgsGuimodifyStagebuttonFold")
-					delete pelement.stagebutton;
-					api.log("stagebutton removed");
-				}
-			}
+			configButton({
+				buttonid: "foldspriteinfo",
+				enable: foption.foldspriteinfo,
+				styles: {
+					left: 0,
+					top: 0,
+					position: "absolute"
+				},
+				addinner: "▲",
+				removeinner: "▼",
+				targetcss: "sprite-selector_scroll-wrapper_",
+				cssname: "tfgsGuimodifySpriteinfoFold"
+			});
 
-			/* ----------------- */
-			if (foption.foldthestage) {
-				/* ------ foldthestage:on ------ */
-				if (pelement.thestage === undefined) {
-					let thestage = selectElement("stage-wrapper_stage-canvas-wrapper_");
-					if (thestage !== null) {
-						if (pelement.foldthestagebutton === undefined) {
-							let button = pelement.foldthestagebutton = createButton(MARK1, "0", "0");
-							button.style.left = "unset";
-							button.style.right = "calc(2.5rem + 2px)";
-							pbutton.foldthestagebutton = false;
-							button.innerText = "△";
-							button.addEventListener("click", function() {
-								if (!pbutton.foldthestagebutton) {
-									button.innerText = "▽";
-									thestage.classList.add("tfgsGuimodifyStageFold")
-								} else {
-									button.innerText = "△";
-									thestage.classList.remove("tfgsGuimodifyStageFold")
-								}
-								pbutton.foldthestagebutton = !pbutton.foldthestagebutton;
-							});
-							api.log("thestage is OK");
-						}
-						pelement.thestage = thestage;
-					} else {
-						api.warn("thestage not found.");
-					}
-				}
-			} else {
-				/* ------ foldthestage:off ------ */
-				if (pelement.thestage !== undefined) {
-					let thestage = pelement.thestage;
-					if (pelement.foldthestagebutton !== undefined) {
-						pelement.foldthestagebutton.remove();
-						delete pelement.foldthestagebutton;
-					}
-					thestage.classList.remove("tfgsGuimodifyStageFold")
-					delete pelement.thestage;
-					api.log("thestage removed");
-				}
-			}
+			configButton({
+				buttonid: "foldstagebutton",
+				enable: foption.foldstagebutton,
+				styles: {
+					right: 0,
+					top: "calc(2rem + 1px)",
+					position: "absolute"
+				},
+				addinner: "▶",
+				removeinner: "◀",
+				targetcss: "sprite-selector_scroll-wrapper_",
+				cssname: "tfgsGuimodifyStagebuttonFold"
+			});
 
-			/* ----------------- */
+			configButton({
+				buttonid: "foldthestage",
+				enable: foption.foldthestage,
+				styles: {
+					right: 0,
+					top: 0,
+					position: "absolute"
+				},
+				addinner: "▲",
+				removeinner: "▼",
+				targetcss: "sprite-selector_scroll-wrapper_",
+				cssname: "tfgsGuimodifyStageFold"
+			});
 		} catch (e) {
 			api.error(e);
 		}
@@ -298,27 +204,32 @@
 		option: {
 			foldmenu: {
 				type: "check",
-				name: "折叠菜单按钮",
+				name: "折叠菜单",
 				default: true
 			},
 			foldstagetarget: {
 				type: "check",
-				name: "折叠舞台按钮",
+				name: "折叠舞台和角色列表",
 				default: true
 			},
 			foldspriteinfo: {
 				type: "check",
-				name: "折叠sprite按钮",
+				name: "折叠角色参数",
 				default: true
 			},
 			foldstagebutton: {
 				type: "check",
-				name: "折叠stage按钮",
+				name: "折叠舞台按钮",
 				default: true
 			},
 			foldthestage: {
 				type: "check",
-				name: "折叠stage",
+				name: "折叠舞台",
+				default: true
+			},
+			expand100: {
+				type: "check",
+				name: "填满屏幕(实验)",
 				default: true
 			}
 		},
