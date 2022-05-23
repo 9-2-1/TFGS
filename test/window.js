4a90c9dcfdd3a57f94ef3b11f8863a03a17a574f
six-6 <1195132772@qq.com>
2022-05-23 19:47:04 +0800

    窗口最大化最小化还原关闭功能和回调函数完成

--- test/window.js
+++ test/window.js
@@ -5,13 +5,24 @@ tfgswindow = tfgs.window.create({
 	canMove: true,
 	canResize: true,
 	canClose: true,
-	canZoom: true,
-	top: 0,
-	left: 0,
+	x: 20,
+	y: 20,
 	width: 200,
 	height: 200,
 	minWidth: 100,
-	minHeight: 100
+	minHeight: 0,
+	cnt: 0,
+	onMove: function() {
+		this.title = (this.isMinimize ? "Minimized" : this.isMaximize ? "Maximized" : "Moved") + ` (${Date()} x${++this.cnt}`;
+		this._refresh();
+	},
+	onResize: function() {
+		this.title = (this.isMinimize ? "Minimized" : this.isMaximize ? "Maximized" : "Resized") + ` (${Date()} x${++this.cnt}`;
+		this._refresh();
+	},
+	onClose: function() {
+		return confirm("X ?");
+	},
 });
 
 /*
--- tfgs/window.js
+++ tfgs/window.js
@@ -2,201 +2,265 @@ tfgs.window = {};
 
 tfgs.window.create = function(options) {
 	let windowobj = {
+		/* elements */
 		titleDiv: null,
 		innerDiv: null,
 		windowDiv: null,
-		top: 0,
-		left: 0,
+		resizeDiv: null,
+
+		/* position */
+		x: 0,
+		y: 0,
+		width: 400,
 		height: 300,
-		width: 300,
-		margin: 0,
+		minHeight: 0,
+		minWidth: 100,
+
+		/* status */
 		isMinimize: false,
 		isMaximize: false,
+		posRestore: {
+			x: 0,
+			y: 0,
+			width: 400,
+			height: 300
+		},
+		title: "",
+
+		/* options */
 		canMinimize: true,
 		canMaximize: true,
 		canClose: true,
 		canResize: true,
 		canMove: true,
-		onMinimize: function() {},
-		onMaximize: function() {},
+
+		/* callback */
 		onClose: function() {},
 		onResize: function() {},
 		onMove: function() {},
-		minimize: function() {},
-		maximize: function() {},
-		close: function() {},
-		resize: function() {},
-		move: function() {}
+
+		/* functions */
+		_rememberPos: function() {
+			if (!this.isMinimize && !this.isMaximize) {
+				this.posRestore.x = this.x;
+				this.posRestore.y = this.y;
+				this.posRestore.width = this.width;
+				this.posRestore.height = this.height;
+			}
+		},
+		minimize: function() {
+			this._rememberPos();
+			this.isMinimize = true;
+			this.isMaximize = false;
+			this._refresh();
+			this.onResize();
+			this.onMove();
+		},
+		maximize: function() {
+			this._rememberPos();
+			this.isMinimize = false;
+			this.isMaximize = true;
+			this._refresh();
+			this.onResize();
+			this.onMove();
+		},
+		restore: function() {
+			this.isMinimize = false;
+			this.isMaximize = false;
+			this.x = this.posRestore.x;
+			this.y = this.posRestore.y;
+			this.width = this.posRestore.width;
+			this.height = this.posRestore.height;
+			this._refresh();
+			this.onResize();
+			this.onMove();
+		},
+		movetotop: function() {
+			this.windowDiv.remove();
+			document.body.appendChild(this.windowDiv);
+		},
+		close: function() {
+			if (this.onClose() === false) return;
+			this.windowDiv.remove();
+			this.windowDiv = null;
+		},
+		resize: function(w, h) {
+			if (w === this.width && h === this.height) return;
+			this.width = w;
+			this.height = h;
+			this._refresh();
+			this.onResize();
+		},
+		move: function(x, y) {
+			if (x === this.x && y === this.y) return;
+			this.x = x;
+			this.y = y;
+			this._refresh();
+			this.onMove();
+		},
+		_refresh: function() {
+			let sX = window.innerWidth,
+				sY = window.innerHeight;
+			if (this.isMaximize) {
+				this.x = 0;
+				this.y = 0;
+				this.width = sX;
+				this.height = sY;
+			} else {
+				let mX = this.minWidth + 6,
+					mY = this.minHeight + 26,
+					x = this.x,
+					y = this.y,
+					w = this.width,
+					h = this.height;
+				if (this.isMinimize) {
+					w = mX;
+					h = 26;
+				} else {
+					if (w < mX) w = mX;
+					if (h < mY) h = mY;
+					if (w > sX) w = sX;
+					if (h > sY) h = sY;
+				}
+
+				let X = x + w,
+					Y = y + h;
+				if (x < 0) x = 0;
+				if (y < 0) y = 0;
+				if (X > sX) x -= X - sX;
+				if (Y > sY) y -= Y - sY;
+
+				this.x = x;
+				this.y = y;
+				this.width = w;
+				this.height = h;
+			}
+
+			let styl = this.windowDiv.style;
+			styl.left = this.x + "px";
+			styl.top = this.y + "px";
+			styl.width = this.width + "px";
+			styl.height = this.height + "px";
+
+			showhide(this.titleDiv.children[0],
+				!this.isMinimize);
+			this.titleDiv.children[1].innerText = this.title;
+			showhide(this.titleDiv.children[2],
+				!this.isMinimize && this.canMinimize);
+			showhide(this.titleDiv.children[3],
+				this.isMaximize);
+			showhide(this.titleDiv.children[4],
+				!this.isMinimize && !this.isMaximize && this.canMaximize);
+			showhide(this.titleDiv.children[5],
+				this.canClose);
+			showhide(this.innerDiv,
+				!this.isMinimize);
+			showhide(this.resizeDiv,
+				!this.isMinimize && !this.isMaximize && this.canResize);
+		}
 	};
 
+	windowobj = Object.assign(windowobj, options);
 	let windowDiv = element("div", "tfgsWindow");
 	windowDiv.innerHTML = `
 <div class="tfgsWindowTitle">
 	<span>≡</span>
-	<span class="tfgsWindowText">Titkjvhkvkvojvpjv9jvojbole</span>
+	<span class="tfgsWindowText"></span>
 	<span>_</span>
 	<span>▭</span>
 	<span>□</span>
 	<span>✕</span>
 </div>
-<div class="tfgsWindowContent">
-	<pre>
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	pre
-	</pre>
-</div>
+<div class="tfgsWindowContent"></div>
 <div class="tfgsWindowResize"></div>
 `;
 	document.body.appendChild(windowDiv);
 
 	window.addEventListener("resize", function() {
-		frameinscreen(windowDiv);
+		windowobj._refresh();
+		windowobj.onResize();
 	});
 
-	windowDiv.children[0].children[2].addEventListener("click", function() {
-		windowDiv.style.width = "150px";
-		windowDiv.style.height = "26px";
-		frameinscreen(windowDiv);
+	let titleDiv = windowDiv.children[0];
+	let innerDiv = windowDiv.children[1];
+	let resizeDiv = windowDiv.children[2];
+
+	let titleDivs = titleDiv.children;
+
+	// 不能写成
+	// windowDiv.addEventListener("mousedown", windowobj.movetotop, true);
+	// 否则里面的this会指向windowDiv而不是windowobj
+
+	windowDiv.addEventListener("mousedown", function(event) {
+		windowobj.movetotop();
+	}, true);
+
+	titleDivs[2].addEventListener("click", function(event) {
+		windowobj.minimize();
 	});
 
-	windowDiv.children[0].children[3].addEventListener("click", function() {
-		windowDiv.style.width = "200px";
-		windowDiv.style.height = "150px";
-		frameinscreen(windowDiv);
+	titleDivs[3].addEventListener("click", function(event) {
+		windowobj.restore();
 	});
 
-	windowDiv.children[0].children[4].addEventListener("click", function() {
-		windowDiv.style.width = "1000000px";
-		windowDiv.style.height = "1000000px";
-		frameinscreen(windowDiv);
+	titleDivs[4].addEventListener("click", function(event) {
+		windowobj.maximize();
 	});
 
-	windowDiv.children[0].children[5].addEventListener("click", function() {
-		windowDiv.remove();
+	titleDivs[5].addEventListener("click", function(event) {
+		windowobj.close();
 	});
 
-	tfgs.drag.setdrag(windowDiv.children[0].children[1], {
+	let moveObj = {
 		onStart: function(event) {
-			if (event.target.tagName === "INPUT") return null;
-			let css = window.getComputedStyle(windowDiv);
-			let x = cssnum(css.left);
-			let y = cssnum(css.top);
-			frameinscreen(windowDiv);
+			if (windowobj.isMaximize)
+				return null;
+			windowobj.movetotop();
 			return {
-				x: x,
-				y: y
+				x: windowobj.x,
+				y: windowobj.y
 			};
 		},
 		onMove: function(x, y, event) {
-			windowDiv.style.left = x + "px";
-			windowDiv.style.top = y + "px";
-			frameinscreen(windowDiv);
+			if (windowobj.canMove)
+				windowobj.move(x, y);
+		},
+		onEnd: function(mode, event) {
+			if (windowobj.isMinimize && mode === "click")
+				windowobj.restore();
+		}
+	};
+
+	tfgs.drag.setdrag(titleDivs[0], moveObj);
+	tfgs.drag.setdrag(titleDivs[1], moveObj);
+
+	tfgs.drag.setdrag(resizeDiv, {
+		onStart: function(event) {
+			return {
+				x: windowobj.width,
+				y: windowobj.height
+			};
+		},
+		onMove: function(x, y, event) {
+			windowobj.resize(x, y);
 		},
 		onEnd: function(mode, event) {}
 	});
 
-	tfgs.drag.setdrag(windowDiv.children[2], {
-		onStart: function(event) {
-			let css = window.getComputedStyle(windowDiv);
-			let x = cssnum(css.width);
-			let y = cssnum(css.height);
-			return {
-				x: x,
-				y: y
-			};
-		},
-		onMove: function(x, y, event) {
-			if (x < 100) x = 100;
-			if (y < 26) y = 26;
-			windowDiv.style.width = x + "px";
-			windowDiv.style.height = y + "px";
-			frameinscreen(windowDiv);
-		},
-		onEnd: function(mode, event) {}
-	});
+	windowobj.windowDiv = windowDiv;
+	windowobj.titleDiv = titleDiv;
+	windowobj.innerDiv = innerDiv;
+	windowobj.resizeDiv = resizeDiv;
+
+	windowobj._refresh();
+
+	return windowobj;
 };
+
+function showhide(x, show) {
+	x.style.display = show ? "inherit" : "none";
+}
 
 function cssnum(x) {
 	return Number(/[-.\d]*(?:e[+-]?\d+)?/.exec(x)[0]);
-}
-
-function frameinscreen(elem) {
-	let css = window.getComputedStyle(elem);
-	let sX = window.innerWidth;
-	let sY = window.innerHeight;
-	let x = cssnum(css.left);
-	let y = cssnum(css.top);
-	let w = cssnum(css.width);
-	let h = cssnum(css.height);
-	let ch = false;
-	if (w > sX) {
-		w = sX;
-		ch = true;
-	}
-	if (h > sY) {
-		h = sY;
-		ch = true;
-	}
-	let X = x + w,
-		Y = y + h;
-	if (x < 0) {
-		x = 0;
-		ch = true;
-	}
-	if (y < 0) {
-		y = 0;
-		ch = true;
-	}
-	if (X > sX) {
-		x -= X - sX;
-		ch = true;
-	}
-	if (Y > sY) {
-		y -= Y - sY;
-		ch = true;
-	}
-	if (ch) {
-		elem.style.left = x + "px";
-		elem.style.top = y + "px";
-		elem.style.width = w + "px";
-		elem.style.height = h + "px";
-	}
 }
