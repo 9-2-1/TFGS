b676ee2561b26c0c6fea8e207f753ca43338a4b8
six-6 <1195132772@qq.com>
2022-05-24 10:04:38 +0800

    使用tfgs.window，之后就可以不用每次都自己做窗口了

--- tfgs/log.js
+++ tfgs/log.js
@@ -64,108 +64,40 @@ tfgs.log.display = function(div, fliter) {
 tfgs.log.create = function() {
 	try {
 		if (tfgs.log.dispIntv !== null) {
+			tfgs.log.logwin.movetotop();
 			return;
 		}
-		let element = function(tagName, className, type) {
-			let ele = document.createElement(tagName);
-			if (className !== undefined) ele.className = className;
-			ele.className = className;
-			if (type !== undefined) ele.type = type;
-			return ele;
-		};
-		let logdiv = tfgs.log.logdiv = element("div", "tfgsLog");
+		let logwin = tfgs.log.logwin = tfgs.window.create({
+			title: "日志",
+			width: 200,
+			height: 200,
+			minWidth: 100,
+			minHeight: 50,
+			minimizeWidth: 70
+		});
+		let logdiv = logwin.innerDiv;
 		logdiv.innerHTML = `
 <div class="tfgsLogContent"></div>
 <div class="tfgsLogButtons">
-	<span class="tfgsButton">拖动</span>
-	<span class="tfgsButton tfgsRight">关闭</span>
 	<span class="tfgsButton tfgsRight">清空</span>
 </div>
 `;
-		logdiv.style.left = "20px";
-		logdiv.style.top = "20px";
 		let contentdiv = logdiv.children[0];
 		let buttondiv = logdiv.children[1];
-		let bmove = buttondiv.children[0];
-		let bclose = buttondiv.children[1];
-		let bclear = buttondiv.children[2];
+		let bclear = buttondiv.children[0];
 		tfgs.log.dispIntv = tfgs.log.displayInterval(contentdiv, {
 			"name": null,
 			"color": null
 		});
-		let dx, dy, dm = false;
-		let _px = function(x) {
-			return Number(x.slice(0, -2));
-		};
-		let dragstart = function(x, y) {
-			dx = x - _px(logdiv.style.left);
-			dy = y - _px(logdiv.style.top);
-			dm = true;
-			bmove.classList.add("tfgsDrag");
-		};
-		let dragmove = function(x, y) {
-			if (dm) {
-				logdiv.style.left = x - dx + "px";
-				logdiv.style.top = y - dy + "px";
-				object_in_window(logdiv, 0);
-				return true;
-			}
-			return false;
-		};
-		let dragend = function() {
-			dm = false;
-			bmove.classList.remove("tfgsDrag");
-		};
-		bmove.addEventListener("mousedown", function(event) {
-			dragstart(event.clientX, event.clientY);
-			event.preventDefault();
-		});
-		bmove.addEventListener("touchstart", function(event) {
-			dragstart(event.targetTouches[0].clientX, event.targetTouches[0].clientY);
-			event.preventDefault();
-		});
-		window.addEventListener("mousemove", function(event) {
-			if (dragmove(event.clientX, event.clientY))
-				event.preventDefault();
-		});
-		bmove.addEventListener("touchmove", function(event) {
-			if (dragmove(event.targetTouches[0].clientX, event.targetTouches[0].clientY))
-				event.preventDefault();
-		});
-		bmove.addEventListener("touchend", function(event) {
-			dragend();
-		});
-		window.addEventListener("mouseup", function(event) {
-			dragend();
-		});
-		window.addEventListener("mouseleave", function(event) {
-			dragend();
-		});
 		bclear.addEventListener("click", function() {
 			tfgs.log.clear();
 		});
-		bclose.addEventListener("click", function() {
+		logwin.onClose = function() {
 			clearInterval(tfgs.log.dispIntv);
 			logdiv.remove();
 			tfgs.log.dispIntv = null;
-		});
-		window.addEventListener("resize", function(event) {
-			object_in_window(logdiv, 0);
-		});
-		document.body.appendChild(logdiv);
+		};
 	} catch (e) {
 		tfgs.error(e);
 	}
 };
-
-function object_in_window(obj, pad) {
-	let objrect = obj.getBoundingClientRect();
-	if (objrect.right > window.innerWidth - pad)
-		obj.style.left = (window.innerWidth - objrect.width - pad) + "px";
-	if (objrect.left < 0 + pad)
-		obj.style.left = (0 + pad) + "px";
-	if (objrect.bottom > window.innerHeight - pad)
-		obj.style.top = (window.innerHeight - objrect.height - pad) + "px";
-	if (objrect.top < 0 + pad)
-		obj.style.top = (0 + pad) + "px";
-}
