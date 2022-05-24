fab22284ea5f949f25ba8eb0e86565cd3d496f05
six-6 <1195132772@qq.com>
2022-05-24 11:07:00 +0800

    窗口活用示例

---
+++ functions/windowf___.js
@@ -0,0 +1,47 @@
+let windowf___;
+tfgs.func.add({
+	id: "windowf___",
+	name: "window f___",
+	onenable: function(_api) {
+		windowf___ = [];
+		api = _api;
+		windowf___.push(tfgs.window.create({
+			title: "Grow",
+			canMaximize: false,
+			canMinimize: false,
+			canResize: false,
+			height: 130,
+			width: 130,
+			onClose: conti
+		}));
+	},
+	ondisable: function() {
+		for (let i in windowf___) {
+			windowf___[i].onClose = function() {};
+			windowf___[i].close();
+		}
+	},
+	onoption: function() {}
+});
+
+function conti() {
+	windowf___.splice(windowf___.indexOf(this), 1);
+	windowf___.push(tfgs.window.create({
+		title: "grow",
+		canMaximize: false,
+		canMinimize: false,
+		canResize: false,
+		height: 130,
+		width: 130,
+		onClose: conti
+	}));
+	windowf___.push(tfgs.window.create({
+		title: "grow",
+		canMaximize: false,
+		canMinimize: false,
+		canResize: false,
+		height: 130,
+		width: 130,
+		onClose: conti
+	}));
+}
