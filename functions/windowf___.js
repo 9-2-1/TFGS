a295567f9e4cfc1b9b31970095c53d4270323050
six-6 <1195132772@qq.com>
2022-05-25 15:02:37 +0800

    Teriteri

--- functions/windowf___.js
+++ functions/windowf___.js
@@ -4,15 +4,7 @@ tfgs.func.add({
 	name: "window f___",
 	onenable: function(_api) {
 		windowf___ = [];
-		windowf___.push(tfgs.window.create({
-			title: "Grow",
-			canMaximize: false,
-			canMinimize: false,
-			canResize: false,
-			height: 130,
-			width: 130,
-			onClose: conti
-		}));
+		newwin();
 	},
 	ondisable: function() {
 		for (let i in windowf___) {
@@ -23,24 +15,23 @@ tfgs.func.add({
 	onoption: function() {}
 });
 
+function newwin() {
+	let win = tfgs.window.create({
+		title: "Teriteri",
+		canMaximize: false,
+		canMinimize: false,
+		canResize: false,
+		height: 130,
+		width: 130,
+		onClose: conti
+	});
+	win.innerDiv.style = "vertical-align:middle;text-align:center;font-size:80px;";
+	win.innerDiv.innerText = "☆";
+	windowf___.push(win);
+}
+
 function conti() {
 	windowf___.splice(windowf___.indexOf(this), 1);
-	windowf___.push(tfgs.window.create({
-		title: "grow",
-		canMaximize: false,
-		canMinimize: false,
-		canResize: false,
-		height: 130,
-		width: 130,
-		onClose: conti
-	}));
-	windowf___.push(tfgs.window.create({
-		title: "grow",
-		canMaximize: false,
-		canMinimize: false,
-		canResize: false,
-		height: 130,
-		width: 130,
-		onClose: conti
-	}));
+	newwin();
+	newwin();
 }
